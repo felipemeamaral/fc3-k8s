a0247741e658f4baf1b49949f8238faf5fa0c745
Felipe Amaral <pulga.lipe@gmail.com>
2023-01-23 21:52:38 -0300

    chore: removed test code

--- server.go
+++ server.go
@@ -8,5 +8,5 @@ func main() {
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Hello Full Cycle</h1>< br/><h1>Baby! \o/</h1>"))
+	w.Write([]byte("<h1>Hello Full Cycle</h1>"))
 }
