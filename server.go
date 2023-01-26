6975ce6d487cf17a3c31621fb8326b9107f54e33
Felipe Amaral <pulga.lipe@gmail.com>
2023-01-25 23:07:01 -0300

    chore: added configmap, volume mount and a new path on server app

--- server.go
+++ server.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
+	http.HandleFunc("/configmap", ConfigMap)
 	http.HandleFunc("/", Hello)
 	http.ListenAndServe(":8000", nil)
 }
@@ -15,6 +18,16 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("NAME")
 	age := os.Getenv("AGE")
 	message := fmt.Sprintf("Hello I'm %s. I'm %s.", name, age)
+
+	w.Write([]byte(message))
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("/app/myfamily/family.txt")
+	if err != nil {
+		log.Fatal("Error reading file", err)
+	}
+	message := fmt.Sprintf("My family: %s.", string(data))
 
 	w.Write([]byte(message))
 }
